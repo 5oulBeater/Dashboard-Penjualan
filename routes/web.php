7a36d110b760200e1c7bd1904628efc8661ec4d2
5oulBeater <ammarzuhdi46@gmail.com>
2025-12-03 11:21:50 +0700

    fix run-migrate route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+// sementara untuk migrate Railway
+Route::get('/run-migrate', function () {
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        Artisan::call('db:seed', ['--force' => true]);
+        return "Migration success!";
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    }
+});
